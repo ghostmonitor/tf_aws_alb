256757ed6af1135845479f82bb8616fbafa57156
David Papp <pappdav@gmail.com>
2017-08-11 21:14:43 +0200

    :sparkles: Add more output

--- alb/outputs.tf
+++ alb/outputs.tf
@@ -6,6 +6,10 @@ output "alb_dns_name" {
   value = "${aws_alb.main.dns_name}"
 }
 
+output "alb_name" {
+  value = "${aws_alb.main.name}"
+}
+
 output "alb_zone_id" {
   value = "${aws_alb.main.zone_id}"
 }
