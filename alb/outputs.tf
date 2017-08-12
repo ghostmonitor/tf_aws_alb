c208e10faf79f13855e61760ed69f09d678e5acb
David Papp <pappdav@gmail.com>
2017-08-12 20:21:07 +0200

    :sparkles: Add new output

--- alb/outputs.tf
+++ alb/outputs.tf
@@ -14,6 +14,10 @@ output "alb_zone_id" {
   value = "${aws_alb.main.zone_id}"
 }
 
+output "target_group_name" {
+  value = "${aws_alb_target_group.target_group.name}"
+}
+
 output "target_group_arn" {
   value = "${aws_alb_target_group.target_group.arn}"
 }
